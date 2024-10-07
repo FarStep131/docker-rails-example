74da764fb4b8990cf8d8fd94a17f11d874c1d5da
rurero2 <0121mrei@gmail.com>
2024-10-08 03:24:33 +0900

    [Add]決済システム導入

--- app/helpers/customer/cart_items_helper.rb
+++ app/helpers/customer/cart_items_helper.rb
@@ -1,5 +1,5 @@
 module Customer::CartItemsHelper
-    def total_quantity(cart_items)
-      cart_items.sum(:quantity)
-    end
+  def total_quantity(cart_items)
+    cart_items.sum(:quantity)
   end
+end
