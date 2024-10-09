fc0698eb4c8f3c6217a5e5c73d477bbe5a8e48de
rurero2 <0121mrei@gmail.com>
2024-10-09 10:47:13 +0900

    [Add]購入履歴機能と顧客管理画面を実装

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
   }
   root to: 'pages#home'
   namespace :admin do
+    root to: 'pages#home'
     resources :products, only: %i[index show new create edit update]
+    resources :orders, only: %i[show update]
+    resources :customers, only: %i[index show update]
   end
   scope module: :customer do
     resources :products, only: %i[index show]
@@ -20,6 +23,11 @@ Rails.application.routes.draw do
     end
     resources :checkouts, only: [:create]
     resources :webhooks, only: [:create]
+    resources :orders, only: %i[index show] do
+      collection do
+        get 'success'
+      end
+    end
   end
 
   get '/up/', to: 'up#index', as: :up
